09b1ae461b7ca81e39e9bce075f6c5f17439f84b
Irshad Chohan <irshad.comp@gmail.com>
2020-09-02 15:29:17 +0530

    Update WebConfigurer.java

--- src/main/java/org/jhipster/blog/config/WebConfigurer.java
+++ src/main/java/org/jhipster/blog/config/WebConfigurer.java
@@ -66,7 +66,8 @@ public class WebConfigurer implements ServletContextInitializer, WebServerFactor
         }
         log.info("Web application fully configured");
         try {
-            AWSXRayRecorderBuilder builder = AWSXRayRecorderBuilder.standard().withPlugin(new EC2Plugin()).withPlugin(new ECSPlugin());
+            AWSXRayRecorderBuilder builder = AWSXRayRecorderBuilder.standard().withPlugin(new EC2Plugin()).withPlugin(new ECSPlugin())
+                                                                              .withSegmentListener(new SLF4JSegmentListener("JB-XRAY"));
 
             URL ruleFile = WebConfigurer.class.getResource("/sampling-rules.json");
             builder.withSamplingStrategy(new CentralizedSamplingStrategy(ruleFile));
